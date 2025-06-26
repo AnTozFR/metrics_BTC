d2e09719c581fce87221e17ae56f6d7261bf7895
AnTozFR <39851591+AnTozFR@users.noreply.github.com>
2025-06-26 20:56:48 +0200

    Update metrics_ALTBG.py

--- metrics_ALTBG.py
+++ metrics_ALTBG.py
@@ -4,10 +4,6 @@ import yfinance as yf
 from datetime import datetime
 import math
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/api/metrics')
 def get_metrics():
     # Données fixes
     shares_fully_diluted = 316_207_689
@@ -97,10 +93,3 @@ def get_metrics():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-def get_altbg_metrics():
-    with app.test_request_context():
-        return get_metrics()
-
-if __name__ == '__main__':
-    app.run(debug=True)
