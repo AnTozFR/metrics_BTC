1c8d993b3306299f2562e6d272a9000004e93789
AnTozFR <39851591+AnTozFR@users.noreply.github.com>
2025-06-26 14:48:33 +0200

    Update metrics_MTPLF.py

--- metrics_MTPLF.py
+++ metrics_MTPLF.py
@@ -40,7 +40,7 @@ def get_metrics():
         months_to_cover = days_to_cover / 30 if days_to_cover else None
 
         # Début du programme
-        start_date = datetime.strptime("2024-12-11", "%Y-%m-%d")
+        start_date = datetime.strptime("2024-04-08", "%Y-%m-%d")
         days_since_start = (datetime.today() - start_date).days
 
         # Vitesse d'accumulation linéaire
