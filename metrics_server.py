009296490d808b7c05f59f305b2bc2b560ceb212
AnTozFR <39851591+AnTozFR@users.noreply.github.com>
2025-06-26 14:28:13 +0200

    Update metrics_server.py

--- metrics_server.py
+++ metrics_server.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from metrics_ALTBG import get_altbg_metrics
-from metrics_MTPLF import get_mtplf_metrics
+from metrics_MTPLF import get_mtplf_metrics  # Assure-toi que celle-ci existe aussi
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/altbg")
 def altbg():
-    return jsonify(get_altbg_metrics())
+    return get_altbg_metrics()
 
 @app.route("/mtplf")
 def mtplf():
-    return jsonify(get_mtplf_metrics())
+    return get_mtplf_metrics()
+
+if __name__ == '__main__':
+    app.run(debug=True)
