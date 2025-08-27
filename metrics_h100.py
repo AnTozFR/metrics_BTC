cacdc3d0108ea1bb12d1c2b67de53d760615f404
AnTozFR <39851591+AnTozFR@users.noreply.github.com>
2025-08-27 18:40:39 +0200

    Update metrics_h100.py

--- metrics_h100.py
+++ metrics_h100.py
@@ -7,7 +7,7 @@ import math
 def get_metrics():
     # Données fixes
     shares_fully_diluted = 352_950_000
-    btc_held = 911.29
+    btc_held = 957.5
     btc_yield_ytd = 6785
     q2_yield = 1129
     debt_btc = 0
@@ -29,7 +29,8 @@ def get_metrics():
         {"date": "2025-08-01", "btc": 17.47, "price": 116965},
         {"date": "2025-08-06", "btc": 60.60, "price": 115190},
         {"date": "2025-08-13", "btc": 45.80, "price": 119085},
-        {"date": "2025-08-20", "btc": 102.19,"price": 120538}
+        {"date": "2025-08-20", "btc": 102.19,"price": 120538},
+        {"date": "2025-08-27", "btc": 46.22,"price": 112589}
     ]
 
     try:
