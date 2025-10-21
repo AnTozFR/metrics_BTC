b6b5d56e7854fc6ca5a75fb89d8274afa9d9850d
AnTozFR <39851591+AnTozFR@users.noreply.github.com>
2025-10-21 23:30:28 +0200

    Update metrics_MSTR.py

--- metrics_MSTR.py
+++ metrics_MSTR.py
@@ -6,12 +6,12 @@ import math
 
 def get_metrics():
     # Données fixes
-    shares_fully_diluted = 320_067_000
-    btc_held = 640250
+    shares_fully_diluted = 320_071_000
+    btc_held = 640418
     btc_yield_ytd = 26
     q2_yield = 5.2
     debt_btc = 0
-    debt_fiat = 14_828_000_000
+    debt_fiat = 14_847_000_000
 
     btc_history = [
     {"date": "2020-08-10", "btc": 21454, "price": 11652},
@@ -96,6 +96,7 @@ def get_metrics():
     {"date": "2025-09-22", "btc": 850,   "price": 117344},
     {"date": "2025-09-29", "btc": 196,   "price": 113048},
     {"date": "2025-10-13", "btc": 220,   "price": 123561},
+    {"date": "2025-10-20", "btc": 168,   "price": 112051},
     ]
 
     try:
@@ -263,6 +264,7 @@ def get_metrics():
 
 def get_mstr_metrics():
     return get_metrics()
+
 
 
 
